e02195a508440b4bfe4dd925aa77c47dfd6893a3
mybec <my.beck.for.work@mail.ru>
2021-12-18 13:31:31 +0500

    First practise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './styles/App.css'
+import FramesList from "./components/FramesList";
+import FrameForm from "./components/FrameForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [frames, setFrames] = useState([
+        {id: 1, scene_id: 1},
+        {id: 2, scene_id: 1},
+        {id: 3, scene_id: 1},
+        {id: 4, scene_id: 2},
+        {id: 5, scene_id: 2},
+    ])
+
+    const addFrame = (newFrame) => {
+        setFrames([...frames, newFrame])
+    }
+
+    return (
+        <div className="App">
+            <FramesList frames={frames}/>
+            <FrameForm cb__add_frame={addFrame} frames_count={frames.length}/>
+        </div>
   );
 }
 
